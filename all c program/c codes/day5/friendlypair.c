f6d62a7903b402a61f3596c737670592a77118b3
einiyaselvie <einiyaselvielavarasan@gmail.com>
2025-07-22 23:51:37 +0530

    add

--- all c program/c codes/day5/friendlypair.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int n1, n2;
-    
-
-    return 0;
-}
--- all c program/problemsolving/mangotree1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int r, c, t;
-    scanf("%d%d%d", &r, &c, &t);
-
-    
-
-    
